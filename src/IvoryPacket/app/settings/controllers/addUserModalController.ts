ff646b34cc17c14d1b9e99de8b654f79fe1b6f3e
Blake Mumford <blake.mumford@indoctrinate.com.au>
2016-05-27 07:49:23 +1000

    Adding users

---
+++ src/IvoryPacket/app/settings/controllers/addUserModalController.ts
@@ -0,0 +1,10 @@
+﻿module settings.controllers {
+    export class AddUserModalController {
+        static $inject: Array<string> = [];
+        constructor() {
+
+        }
+
+    }
+    angular.module("settings").controller("AddUserModalController", AddUserModalController);
+}
--- src/IvoryPacket/app/settings/controllers/usersListController.ts
+++ src/IvoryPacket/app/settings/controllers/usersListController.ts
@@ -1,7 +1,17 @@
 ﻿module settings.controllers {
-    export class UsersListController{
-        static $inject: Array<string> = [];
+    export class UsersListController {
+        static $inject: Array<string> = ["$uibModal"];
+        constructor(private $uibModal: angular.ui.bootstrap.IModalService) {
 
+        }
+        addNewUser() {
+            this.$uibModal.open({
+                templateUrl: "html/add-user-modal.html",
+                backdrop: "static",
+                controllerAs: "vm",
+                controller: ""
+            })
+        }
     }
 
     angular.module("settings").controller("UsersListController", UsersListController)
